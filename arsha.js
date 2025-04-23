becf9a38d741f32e4abbfa9457beccf281bd063e
mubashfaq <mubashfaq10@gmail.com>
2025-04-23 17:18:44 +0500

    inital commit

---
+++ arsha.js
@@ -0,0 +1,164 @@
+$('.icon-toggle3').click(function() {
+    $(this).toggleClass('fa-chevron-down fa-chevron-up');
+});
+
+document.getElementById('toggleDiv3').addEventListener('click', function() {
+    var icon = this.querySelector('.icon-toggle3');
+    var collapsibleContent3 = document.getElementById('collapsibleContent3');
+    
+    if (collapsibleContent3.classList.contains('show')) {
+        collapsibleContent3.classList.remove('show');
+        icon.classList.remove('fa-chevron-up');
+        icon.classList.add('fa-chevron-down');
+    } else {
+        collapsibleContent3.classList.add('show');
+        icon.classList.remove('fa-chevron-down');
+        icon.classList.add('fa-chevron-up');
+    }
+});
+
+$('.icon-toggle1').click(function() {
+    $(this).toggleClass('fa-chevron-down fa-chevron-up');
+});
+
+document.getElementById('toggleDiv1').addEventListener('click', function() {
+    var icon = this.querySelector('.icon-toggle1');
+    var collapsibleContent1 = document.getElementById('collapsibleContent1');
+    
+    if (collapsibleContent1.classList.contains('show')) {
+        collapsibleContent1.classList.remove('show');
+        icon.classList.remove('fa-chevron-up');
+        icon.classList.add('fa-chevron-down');
+    } else {
+        collapsibleContent1.classList.add('show');
+        icon.classList.remove('fa-chevron-down');
+        icon.classList.add('fa-chevron-up');
+    }
+});
+
+$('.icon-toggle1').click(function() {
+    $(this).toggleClass('fa-chevron-down fa-chevron-up');
+});
+
+document.getElementById('toggleDiv2').addEventListener('click', function() {
+    var icon = this.querySelector('.icon-toggle2');
+    var collapsibleContent2 = document.getElementById('collapsibleContent2');
+    
+    if (collapsibleContent2.classList.contains('show')) {
+        collapsibleContent2.classList.remove('show');
+        icon.classList.remove('fa-chevron-up');
+        icon.classList.add('fa-chevron-down');
+    } else {
+        collapsibleContent2.classList.add('show');
+        icon.classList.remove('fa-chevron-down');
+        icon.classList.add('fa-chevron-up');
+    }
+});
+
+$('.icon-toggle1').click(function() {
+    $(this).toggleClass('fa-chevron-down fa-chevron-up');
+});
+
+document.getElementById('toggleDiv4').addEventListener('click', function() {
+    var icon = this.querySelector('.icon-toggle1');
+    var collapsibleContent4 = document.getElementById('collapsibleContent4');
+    
+    if (collapsibleContent4.classList.contains('show')) {
+        collapsibleContent4.classList.remove('show');
+        icon.classList.remove('fa-chevron-up');
+        icon.classList.add('fa-chevron-down');
+    } else {
+        collapsibleContent4.classList.add('show');
+        icon.classList.remove('fa-chevron-down');
+        icon.classList.add('fa-chevron-up');
+    }
+});
+
+$('.icon-toggle1').click(function() {
+    $(this).toggleClass('fa-chevron-down fa-chevron-up');
+});
+
+document.getElementById('toggleDiv5').addEventListener('click', function() {
+    var icon = this.querySelector('.icon-toggle1');
+    var collapsibleContent5 = document.getElementById('collapsibleContent5');
+    
+    if (collapsibleContent5.classList.contains('show')) {
+        collapsibleContent5.classList.remove('show');
+        icon.classList.remove('fa-chevron-up');
+        icon.classList.add('fa-chevron-down');
+    } else {
+        collapsibleContent5.classList.add('show');
+        icon.classList.remove('fa-chevron-down');
+        icon.classList.add('fa-chevron-up');
+    }
+});
+
+$('.icon-toggle1').click(function() {
+    $(this).toggleClass('fa-chevron-down fa-chevron-up');
+});
+
+document.getElementById('toggleDiv6').addEventListener('click', function() {
+    var icon = this.querySelector('.icon-toggle1');
+    var collapsibleContent6 = document.getElementById('collapsibleContent6');
+    
+    if (collapsibleContent6.classList.contains('show')) {
+        collapsibleContent6.classList.remove('show');
+        icon.classList.remove('fa-chevron-up');
+        icon.classList.add('fa-chevron-down');
+    } else {
+        collapsibleContent6.classList.add('show');
+        icon.classList.remove('fa-chevron-down');
+        icon.classList.add('fa-chevron-up');
+    }
+});
+
+$('.icon-toggle1').click(function() {
+    $(this).toggleClass('fa-chevron-down fa-chevron-up');
+});
+
+document.getElementById('toggleDiv7').addEventListener('click', function() {
+    var icon = this.querySelector('.icon-toggle1');
+    var collapsibleContent7 = document.getElementById('collapsibleContent7');
+    
+    if (collapsibleContent7.classList.contains('show')) {
+        collapsibleContent7.classList.remove('show');
+        icon.classList.remove('fa-chevron-up');
+        icon.classList.add('fa-chevron-down');
+    } else {
+        collapsibleContent7.classList.add('show');
+        icon.classList.remove('fa-chevron-down');
+        icon.classList.add('fa-chevron-up');
+    }
+});
+
+$('.icon-toggle1').click(function() {
+    $(this).toggleClass('fa-chevron-down fa-chevron-up');
+});
+
+document.getElementById('toggleDiv8').addEventListener('click', function() {
+    var icon = this.querySelector('.icon-toggle1');
+    var collapsibleContent8 = document.getElementById('collapsibleContent8');
+    
+    if (collapsibleContent8.classList.contains('show')) {
+        collapsibleContent8.classList.remove('show');
+        icon.classList.remove('fa-chevron-up');
+        icon.classList.add('fa-chevron-down');
+    } else {
+        collapsibleContent8.classList.add('show');
+        icon.classList.remove('fa-chevron-down');
+        icon.classList.add('fa-chevron-up');
+    }
+});
+
+
+
+
+function play(){
+    var x = document.getElementById('playy')
+    if(x.style.display === "none"){
+        x.style.display = "block";
+    }
+    else{
+        x.style.display = "none";
+    }
+}
